f2743d89a842f92444abf8786f93a4919f04d0b7
artem.chernomazov <pechenkazz@mail.ru>
2022-09-30 21:19:27 +0300

    добавлено решение

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Result(string[] Array)
+{
+    string[] Array2 = new string[Array.Length];
+    int countOfArray2 = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+             Array2[i] = Array[i];
+             countOfArray2++;
+        }
+        else Array2[i] = "null";
+    }
+     string[] Array3 = new string[countOfArray2];
+     int countOfArray3=0;
+    for(int i = 0; i<Array2.Length; i++)
+    {
+        
+        if(Array2[i] != "null")    
+        {
+        Array3[countOfArray3] = Array2[i];
+        countOfArray3++;
+        }
+    }
+    return Array3;
+    
+}
+
+
+
+string[] StartArray = { "hello", "2", "world", ":-)" };
+string[] FinalArray = Result(StartArray);
+Console.WriteLine("[{0}]", string.Join(",", FinalArray));
